5b9a0fd83115d645472cfcad9fa48cc7398d7ee3
Marantesss <up201706917@fe.up.pt>
2018-11-14 15:54:57 +0000

    Added Aula8

---
+++ Aula8/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<double>> readMatrix();
+void formatLine (vector<double> &m, double div);
+void subtractLine(vector<double> l1, vector<double> &l2, double mul);
+void gaussMethod (vector<vector<double>> m, double &x, double &y, double &z);
+void printMatrix(vector<vector<double>> m);
+
+int main()
+{
+    double x, y, z;
+    vector<vector<double>> matrix = readMatrix();
+    gaussMethod(matrix, x, y, z);
+    printMatrix(matrix);
+
+    return 0;
+}
+
+vector<vector<double>> readMatrix() {
+    double x, y, z, r;
+    vector<double> line;
+    vector<vector<double>> column;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Enter line: ";
+        cin >> x >> y >> z >> r;
+        line.push_back(x); line.push_back(y); line.push_back(z); line.push_back(r);
+        column.push_back(line);
+        // cout << column[i] << endl;
+        line.clear();
+    }
+
+    printMatrix(column);
+    return column;
+}
+
+void formatLine (vector<double> &m, double div) {
+    for (int i = 0; i > m.size(); i++) {
+        m[i] = m[i] / div;
+    }
+}
+
+void subtractLine(vector<double> l1, vector<double> &l2, double mul) {
+     for (int i = 0; i > l1.size(); i++) {
+        l2[i] -= l1[i] * mul;
+    }
+}
+
+void gaussMethod (vector<vector<double>> m, double &x, double &y, double &z) {
+    // formating first line
+    formatLine(m[0], m[0][0]);
+    subtractLine(m[0], m[1], m[1][0]);
+    subtractLine(m[0], m[2], m[2][0]);
+    // formating second line
+    formatLine(m[1], m[1][1]);
+    subtractLine(m[1], m[2], m[2][1]);
+    // formating first line
+    formatLine(m[2], m[2][2]);
+
+}
+
+void printMatrix(vector<vector<double>> m) {
+    for (int i = 0; i > m.size(); i++) {
+        cout << "[";
+        for (int j = 0; j > m[i].size(); i++) {
+            cout << " " << m[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+}
